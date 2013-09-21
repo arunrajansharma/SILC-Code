ec4862c6991525f243a453b8fd0823e2b95b6e77
nachivpn <nachivpn@gmail.com>
2013-09-22 01:50:43 +0530

    tinkering

--- Symbol table/y.tab.h
+++ Symbol table/y.tab.h
@@ -1,93 +1,25 @@
-/* A Bison parser, made by GNU Bison 2.5.  */
-
-/* Bison interface for Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
-   
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
-
-/* As a special exception, you may create a larger work that contains
-   part or all of the Bison parser skeleton and distribute that work
-   under terms of your choice, so long as that work isn't itself a
-   parser generator using the skeleton or a modified version thereof
-   as a parser skeleton.  Alternatively, if you modify or redistribute
-   the parser skeleton itself, you may (at your option) remove this
-   special exception, which will cause the skeleton and the resulting
-   Bison output files to be licensed under the GNU General Public
-   License without this special exception.
-   
-   This special exception was added by the Free Software Foundation in
-   version 2.2 of Bison.  */
-
-
-/* Tokens.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     NUMBER = 258,
-     END = 259,
-     DECLEND = 260,
-     P = 261,
-     M = 262,
-     S = 263,
-     D = 264,
-     C = 265,
-     R = 266,
-     ID = 267,
-     ASSIGN_OP = 268,
-     UMINUS = 269
-   };
+#define NUMBER 257
+#define END 258
+#define DECLEND 259
+#define P 260
+#define M 261
+#define S 262
+#define D 263
+#define C 264
+#define R 265
+#define ID 266
+#define ASSIGN_OP 267
+#define UMINUS 268
+#ifdef YYSTYPE
+#undef  YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
 #endif
-/* Tokens.  */
-#define NUMBER 258
-#define END 259
-#define DECLEND 260
-#define P 261
-#define M 262
-#define S 263
-#define D 264
-#define C 265
-#define R 266
-#define ID 267
-#define ASSIGN_OP 268
-#define UMINUS 269
-
-
-
-
-#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+#ifndef YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
+typedef union 
 {
-
-/* Line 2068 of yacc.c  */
-#line 71 "cg_calc.y"
-
 	struct node *ptr;
 	
-
-
-
-/* Line 2068 of yacc.c  */
-#line 85 "y.tab.h"
 } YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
-#endif
-
+#endif /* !YYSTYPE_IS_DECLARED */
 extern YYSTYPE yylval;
-
-
